e30b42dcd6c0cd669bc3660d55e9ce9b3c8d30d2
Công Danh <congdanh.01.01.2001@gmail.com>
2023-03-03 01:35:01 +0700

    edit component

--- client/src/Components/EmailVerification/Email.jsx
+++ client/src/Components/EmailVerification/Email.jsx
@@ -1,15 +1,31 @@
+import { useDispatch } from 'react-redux'
 import request from '../../utils/axiosConfig'
+import { useNavigate } from 'react-router-dom'
+import { createUser } from '../../redux/apiRequest'
 const { useState } = require('react')
 
 const Email = () => {
     const [code, setCode] = useState('')
+    const [message, setMessage] = useState(null)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    
     console.log(code)
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        const res = await request.post('/auth/verify/register', { code })
-
-        console.log(res.data)
+        // const res = await request.post('/auth/verify/register', { code })
+        const now = new Date().getTime()
+        const expired = localStorage.getItem('expired')
+        const user = JSON.parse(localStorage.getItem('user'))
+        const verificationCode = localStorage.getItem('code')
+        if (now > expired) {
+            console.log(`Token is expired`)
+            setMessage(`Token is expired`)
+        } else {
+            if (code !== verificationCode) setMessage('Invalid code')
+            else createUser(user,dispatch,navigate)
+        }
     }
 
     return (
@@ -21,6 +37,7 @@ const Email = () => {
                     placeholder="your email verification code"
                     onChange={(e) => setCode(e.target.value)}
                 />
+                <h3>{message}</h3>
                 <button type="submit">submit</button>
             </form>
         </div>
--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,24 +1,41 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import "./navbar.css";
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './navbar.css'
+import { useSelector } from 'react-redux'
 const NavBar = () => {
-  const [user,setUSer] = useState(null);
-  return (
-    <nav className="navbar-container">
-      <Link to="/" className="navbar-home"> Home </Link>
-      {user? (
-        <>
-        <p className="navbar-user">Hi, <span> {user}  </span> </p>
-        <Link to="/logout" className="navbar-logout"> Log out</Link>
-        </>
-      ) : (    
-        <>
-      <Link to="/login" className="navbar-login"> Login </Link>
-      <Link to="/register" className="navbar-register"> Register</Link>
-      </>
-)}
-    </nav>
-  );
-};
+    // const [user, setUSer] = useState(null)
+    const user = useSelector((state) => state.auth.login.currentUser)
+    console.log(user)
+    return (
+        <nav className="navbar-container">
+            <Link to="/" className="navbar-home">
+                {' '}
+                Home{' '}
+            </Link>
+            {user ? (
+                <>
+                    <p className="navbar-user">
+                        Hi, <span> {user.accessToken} </span>{' '}
+                    </p>
+                    <Link to="/logout" className="navbar-logout">
+                        {' '}
+                        Log out
+                    </Link>
+                </>
+            ) : (
+                <>
+                    <Link to="/login" className="navbar-login">
+                        {' '}
+                        Login{' '}
+                    </Link>
+                    <Link to="/register" className="navbar-register">
+                        {' '}
+                        Register
+                    </Link>
+                </>
+            )}
+        </nav>
+    )
+}
 
-export default NavBar;
+export default NavBar

0a353fd33b92514d14033d962dd4b5f74aa3c249
Công Danh <congdanh.01.01.2001@gmail.com>
2023-03-11 19:55:56 +0700

    config multer

---
+++ server/src/utils/multer.config.js
@@ -0,0 +1,26 @@
+const multer = require('multer')
+
+const FILE_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpeg',
+    'image/jpg': 'jpg',
+}
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const isValid = FILE_TYPE_MAP[file.mimetype]
+        let uploadError = new Error('Invalid image type')
+        if (isValid) uploadError = null
+        cb(uploadError, 'src/public')
+    },
+    filename: function (req, file, cb) {
+        const fileName = file.originalname.split(' ').join('-')
+        const extension = FILE_TYPE_MAP[file.mimetype]
+        cb(null, `${fileName}-${Date.now()}.${extension}`)
+    },
+})
+
+const uploadOptions = multer({ storage: storage })
+// const uploadOptions = multer({dest:'src/public'})
+
+module.exports = uploadOptions

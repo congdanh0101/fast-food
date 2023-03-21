94bd57559f21a2b34659d3897bc98c2a5820c1fc
Công Danh <congdanh.01.01.2001@gmail.com>
2023-03-21 22:11:22 +0700

    add route

--- server/src/router/router.index.js
+++ server/src/router/router.index.js
@@ -3,17 +3,21 @@ const authRouter = require('./AuthRouter')
 const userRouter = require('./UserRouter')
 const categoryRouter = require('./CategoryRouter')
 const productRouter = require('./ProductRouter')
+const voucherRouter = require('./VoucherRouter')
+const orderRouter = require('./OrderRouter')
 const api = process.env.BASE_API_URL
 const express = require('express')
 // const app = express()
 const errorHandle = require('../middleware/ErrorHandler')
 
 function router(app) {
-  app.use(`${api}auth`, authRouter)
-  app.use(`${api}user`, userRouter)
-  app.use(`${api}category`,categoryRouter)
-  app.use(`${api}product`,productRouter)
-  app.use(errorHandle)
+    app.use(`${api}auth`, authRouter)
+    app.use(`${api}user`, userRouter)
+    app.use(`${api}category`, categoryRouter)
+    app.use(`${api}product`, productRouter)
+    app.use(`${api}voucher`, voucherRouter)
+    app.use(`${api}order`, orderRouter)
+    app.use(errorHandle)
 }
 
 // app.use(`${api}auth`, authRouter)
